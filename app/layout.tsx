c04d81ec6abee1574bde61684cf88c1c643fef43
hareeshmusapnara <harinhhdl143@gmail.com>
2026-06-08 19:18:46 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,16 +17,14 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={`${spaceGrotesk.variable} ${inter.variable} ${ibmPlexMono.variable} antialiased`}>
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7326754333700578" crossOrigin="anonymous" />
+      </head>
       <body style={{ margin: 0, padding: 0 }}>
         <AuthProvider>
           {children}
           <Analytics />
         </AuthProvider>
-        <Script
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7326754333700578"
-          strategy="afterInteractive"
-          crossOrigin="anonymous"
-        />
         <Script
           src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.min.js"
           strategy="beforeInteractive"
